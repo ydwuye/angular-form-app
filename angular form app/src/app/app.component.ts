311030c428ab46f6b150d0a9e004acbe19ab5b77
Youdy Dwuye <30511548+ydwuye@users.noreply.github.com>
2018-02-04 23:18:37 -0500

    uploaded source code
    simple angular form featuring a first name, last name and drop down box with options

---
+++ angular form app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 5';
+
+  onSubmit =function(user) {
+    console.log(user);
+  }
+}
